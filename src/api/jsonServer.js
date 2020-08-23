c6c094b1959377e8894aaa432cb9be0cc46af9b0
Kristina Krapyvina <kristina_krapyvina@epam.com>
2020-08-23 17:15:15 +0300

    Sets up json server

---
+++ src/api/jsonServer.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+  baseURL: 'http://f8e44122845b.ngrok.io'
+});
--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,16 +1,10 @@
 import createDataContext from './createDataContext';
+import jsonServer from '../api/jsonServer';
 
 const blogReducer = (state, action) => {
   switch (action.type) {
-    case 'add_blogpost':
-      return [
-        ...state,
-        {
-          id: Math.floor(Math.random() * 9999),
-          title: action.payload.title,
-          content: action.payload.content,
-        }
-      ];
+    case 'get_blogposts':
+      return action.payload;
     case 'edit_blogpost':
       return state.map(blogPost => {
         return blogPost.id === action.payload.id ? action.payload : blogPost;
@@ -21,28 +15,40 @@ const blogReducer = (state, action) => {
       return state;
   }
 };
+
+const getBlogPosts = dispatch => {
+  return async () => {
+    const response = await jsonServer.get('/blogposts');
+
+    dispatch({type: 'get_blogposts', payload: response.data});
+  };
+};
+
 const addBlogPost = dispatch => {
-  return (title, content, callback) => {
-    dispatch ({type: 'add_blogpost', payload: { title, content }});
+  return async (title, content, callback) => {
+    await jsonServer.post('/blogposts', {title, content});
+
     if(callback) callback();
   };
 };
 
 const editBlogPost = dispatch => {
-  return (id, title, content, callback) => {
+  return async (id, title, content, callback) => {
+    await jsonServer.put(`/blogposts/${id}`, {title, content});
     dispatch ({type: 'edit_blogpost', payload: { id, title, content }});
     if(callback) callback();
   };
 };
 
 const deleteBlogPost = dispatch => {
-  return (id) => {
+  return async (id) => {
+    await jsonServer.delete(`/blogposts/${id}`);
     dispatch ({type: 'delete_blogpost', payload: id});
   };
 };
 
 export const { Context, Provider } = createDataContext(
   blogReducer,
-  { addBlogPost, editBlogPost, deleteBlogPost },
-  [{ id: 1, title: 'Test post', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nibh diam, finibus ac rhoncus eget, tincidunt ac nunc. Curabitur eu tempus diam. Maecenas non tincidunt mi. Aliquam ultrices condimentum felis eu hendrerit. Donec tellus orci, semper a aliquet ut, rutrum sed mi. In hac habitasse platea dictumst. Integer quis eros sit amet sapien rutrum aliquam. Fusce ac pulvinar orci, et tincidunt arcu. Vestibulum luctus viverra turpis, sed congue felis pellentesque consequat. Integer pretium luctus libero nec commodo. Maecenas ante dolor, venenatis a tempus non, sollicitudin eget leo. Maecenas eleifend rhoncus nisl.'}]
+  { getBlogPosts, addBlogPost, editBlogPost, deleteBlogPost },
+  []
 );
